b1e9403117d8873cba2d864d0479cbe2ec553d1e
2018dayanan <daychy72@gmail.com>
2024-01-30 09:35:08 +0545

    Creating Admins

--- admin-panel/index.php
+++ admin-panel/index.php
@@ -1,12 +1,31 @@
-<?php require "layouts/header.php"; ?>    
+<?php require "layouts/header.php"; ?>   
+<?php require "../config/config.php"; ?>
+<?php
+  // jobs count
+  $jobs = $conn->query("SELECT COUNT(*) AS count_jobs FROM jobs");
+  $jobs->execute();
+  $countjobs=$jobs->fetch(PDO::FETCH_OBJ);
+
+  // category count
+  $jobs = $conn->query("SELECT COUNT(*) AS count_categories FROM categories");
+  $jobs->execute();
+  $countcategories=$jobs->fetch(PDO::FETCH_OBJ);
+
+  // admin count
+  $jobs = $conn->query("SELECT COUNT(*) AS count_admins FROM admins");
+  $jobs->execute();
+  $countadmins=$jobs->fetch(PDO::FETCH_OBJ);
+
+
+?> 
 <div class="row">
 
   <div class="col-md-4">
     <div class="card">
             <div class="card-body">
-              <h5 class="card-title"><?php echo $_SESSION['email']; ?></h5>
+              <h5 class="card-title">Jobs</h5>
               <!-- <h6 class="card-subtitle mb-2 text-muted">Bootstrap 4.0.0 Snippet by pradeep330</h6> -->
-              <p class="card-text">number of jobs: 8</p>
+              <p class="card-text">number of jobs: <?php echo $countjobs->count_jobs; ?></p>
              
             </div>
           </div>
@@ -16,7 +35,7 @@
             <div class="card-body">
               <h5 class="card-title">Categories</h5>
               
-              <p class="card-text">number of categories: 4</p>
+              <p class="card-text">number of categories: <?php echo $countcategories->count_categories; ?></p>
               
             </div>
           </div>
@@ -26,7 +45,7 @@
             <div class="card-body">
               <h5 class="card-title">Admins</h5>
               
-              <p class="card-text">number of admins: 3</p>
+              <p class="card-text">number of admins: <?php echo  $countadmins->count_admins; ?></p>
               
             </div>
           </div>
