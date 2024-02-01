25817ab9cc539dfefa1e34397c83a79aecdb317a
2018dayanan <daychy72@gmail.com>
2024-02-01 08:50:08 +0545

    Deleting Categories (Admin-Side)

---
+++ admin-panel/categories-admins/delete-category.php
@@ -0,0 +1,12 @@
+<?php require "../layouts/header.php"; ?>    
+<?php require "../../config/config.php"; ?>
+<?php
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+        $delete = $conn->prepare("DELETE FROM `categories` WHERE id='$id'");
+        $delete->execute();
+        header("location:".ADMINURL."/categories-admins/show-categories.php");
+    }else{
+        header("location:http://localhost/joboard/404.php");
+    }
+?>
